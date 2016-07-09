93191073e9d7c6c903fdf52f85ad659dd1af5f3d
Ethan Lew <ethan.lew@pcc.edu>
2016-07-09 12:39:40 -0700

    Update GLScene.h

--- include/GLScene.h
+++ include/GLScene.h
@@ -1,20 +1,30 @@
-#include <cstdlib>
+#include "Life3d.h"
 #include "life.h"
+
+#include "time.h"
+
 #include <GL\glut.h>
+using namespace life3;
 #ifndef GLSCENE_H
 #define GLSCENE_H
 
+enum Scene
+{
+	scene1,
+	scene2
+};
 
 void GLScene(int argc, char*argv[]);
 void GLScene(int, int, int argc, char*argv[]);
 void Cleanup();
 
+void newLife();
+void newlife3d();
+
 void DisplayGL();
 void KeyboardGL(unsigned char c, int x, int y);
-
-void DrawRectangle(float, float);
+void ReshapeGL(int w, int h);
 
 void render();
-
-void ReshapeGL(int w, int h);
+void render3d();
 #endif
